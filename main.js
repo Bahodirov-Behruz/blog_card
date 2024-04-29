72398bd9af8c7618f10bdd1cbd850b2dcae9ac9c
Behruz <bahodirovbehruz111@gmail.com>
2024-04-29 13:26:57 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+document.body.innerHTML = `
+<div class="container">
+    <div class="wrapper"
+         <div class="box">
+            <div class="img">
+                 <img src="./img/blogimage.svg" alt="">
+                 </div>
+                 <div class="learning">Learning</div>
+                 <div class="data">Published 21 Dec 2023</div>
+                 <h1>HTML & CSS foundations</h1>
+                 <p>These languages are the backbone of every website, defining structure, content, and presentation.</p>
+                 <div class="author">
+                 <img src="./img/userpic.png" alt="">
+                 <p>Greg Hooper</p>
+            </div>
+         </div>
+    </div>
+</div>
+`;
